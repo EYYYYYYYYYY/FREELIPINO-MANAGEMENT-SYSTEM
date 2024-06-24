9d7c392cae5bba58fb79fcf535cd72ac51a2cbe1
SHOGUN9090 <93482660+EYYYYYYYYYY@users.noreply.github.com>
2024-06-24 09:05:41 +0800

    Delete FREELIPINO directory

--- FREELIPINO/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace FREELIPINO.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- FREELIPINO/startup.cs
+++
@@ -1,7 +0,0 @@
-// Startup.cs
-public void ConfigureServices(IServiceCollection services)
-{
-    services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-    services.AddControllersWithViews();
-}
